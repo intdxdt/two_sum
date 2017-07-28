d5ef9556a51c3e9f46f1e8bb9c38acd8d7bbe0ab
Titus Tienaah <titus.intdxdt@gmail.com>
2017-07-27 21:52:38 -0300

    return as vec

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 ///Fast two sum
-pub fn two_sum(a: f64, b: f64) -> (f64, f64) {
+pub fn two_sum(a: f64, b: f64) -> Vec<f64> {
     let x = a + b;
     let bv = x - a;
     let av = x - bv;
     let br = b - bv;
     let ar = a - av;
-    (ar + br, x)
+    vec!(ar + br, x)
 }
 
 
@@ -15,12 +15,12 @@ mod two_sum_test {
 
     #[test]
     fn test_two_sum() {
-        assert!(two_sum(1e+64, 1.0) == (1.0, 1e+64));
-        assert!(two_sum(1., 1.) == (0., 2.));
-        assert!(two_sum(0., -1415.) == (0., -1415.));
-        assert!(two_sum(1e-64, 1e64) == (1e-64, 1e64));
-        assert!(two_sum(0., 0.) == (0., 0.));
-        assert!(two_sum(9e15 + 1., 9e15) == (1., 18000000000000000.));
+        assert_eq!(two_sum(1e+64, 1.0) , [1.0, 1e+64]);
+        assert_eq!(two_sum(1., 1.) , [0., 2.]);
+        assert_eq!(two_sum(0., -1415.) , [0., -1415.]);
+        assert_eq!(two_sum(1e-64, 1e64) , [1e-64, 1e64]);
+        assert_eq!(two_sum(0., 0.) , [0., 0.]);
+        assert_eq!(two_sum(9e15 + 1., 9e15) , [1., 18000000000000000.]);
     }
 }
 
